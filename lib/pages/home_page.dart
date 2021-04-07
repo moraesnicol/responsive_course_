42334e58aa7dcdf83dd6b5e6cf5b38ed27d5a6ca
Gabriel Nicol <moraesnicol@gmail.com>
2021-04-07 18:48:55 -0300

    body init

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:reponsive_course/breakpoints.dart';
 import 'package:reponsive_course/pages/widgets/app_bar/mobile_app_bar.dart';
@@ -9,15 +11,19 @@ class HomePage extends StatelessWidget {
     // ignore: missing_return
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
-        appBar: constraints.maxWidth < mobileBreakpoint
-            ? PreferredSize(
-                child: MobileAppBar(),
-                preferredSize: Size(double.infinity, 56),
-              )
-            : PreferredSize(
-                child: WebAppBar(), preferredSize: Size(double.infinity, 70)),
-        drawer: constraints.maxWidth < mobileBreakpoint ? Drawer() : null,
-      );
+          appBar: constraints.maxWidth < mobileBreakpoint
+              ? PreferredSize(
+                  child: MobileAppBar(),
+                  preferredSize: Size(double.infinity, 56),
+                )
+              : PreferredSize(
+                  child: WebAppBar(), preferredSize: Size(double.infinity, 70)),
+          drawer: constraints.maxWidth < mobileBreakpoint ? Drawer() : null,
+          body: SizedBox(
+            child: Container(
+              color: Colors.amber,
+            ),
+          ));
     });
   }
 }
