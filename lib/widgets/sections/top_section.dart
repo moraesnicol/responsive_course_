d9c505075e636fe23093630eccab12801cf3c92b
Gabriel Nicol <moraesnicol@gmail.com>
2021-04-13 13:25:57 -0300

    top section tablet added.

--- lib/widgets/sections/top_section.dart
+++ lib/widgets/sections/top_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reponsive_course/breakpoints.dart';
 
 import '../custom_search_field.dart';
 
@@ -14,9 +15,12 @@ class TopSection extends StatelessWidget {
             aspectRatio: 3.5,
             child: Stack(
               children: [
-                Image.asset(
-                  'assets/coverfluemy.jpg',
-                  fit: BoxFit.cover,
+                AspectRatio(
+                  aspectRatio: 3.5,
+                  child: Image.asset(
+                    'assets/coverfluemy.jpg',
+                    fit: BoxFit.cover,
+                  ),
                 ),
                 Positioned(
                   left: 50,
@@ -52,6 +56,55 @@ class TopSection extends StatelessWidget {
             ),
           );
         }
+
+        if (maxWidth >= mobileBreakpoint) {
+          return SizedBox(
+            height: 320,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.asset(
+                    'assets/coverfluemy.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 20,
+                  child: Card(
+                      color: Colors.black,
+                      elevation: 8,
+                      child: Container(
+                        padding: const EdgeInsets.all(22),
+                        width: 350,
+                        child: Column(
+                          children: [
+                            Text(
+                              'Aprenda Flutter no seu tempo',
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              'Estude qualquer tema, a qualquer hora. Explore milhares de cursos.',
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                            const SizedBox(height: 8),
+                            CustomSearchField(),
+                          ],
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          );
+        }
+
         return Container();
       },
     );
