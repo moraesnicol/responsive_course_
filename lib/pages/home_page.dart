5e1633e2a806144ea87ac1d7d2cfbea40ee22aa3
Gabriel Nicol <moraesnicol@gmail.com>
2021-04-13 23:27:55 -0300

    .

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
               child: ListView(children: [
                 TopSection(),
                 AdvantagesSection(),
-                
+
               ]),
             ),
           ));
--- lib/widgets/sections/advantages_section.dart
+++ lib/widgets/sections/advantages_section.dart
@@ -4,7 +4,41 @@ class AdvantagesSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget buildAdvantage(String title, String subtitle) {
-      return Wrap(
+      return Row(mainAxisSize: MainAxisSize.min, children: [
+        Icon(
+          Icons.star,
+          color: Colors.white,
+          size: 50,
+        ),
+        const SizedBox(width: 8),
+        Column(
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              subtitle,
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        )
+      ]);
+    }
+
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.fromLTRB(16, 30, 16, 16),
+      decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.white))),
+      child: Wrap(
+        alignment: WrapAlignment.spaceEvenly,
+        runSpacing: 16,
+        spacing: 8,
         children: [
           buildAdvantage(
               '130.000 cursos online', 'Aproveite diversos temas atuais'),
@@ -12,7 +46,7 @@ class AdvantagesSection extends StatelessWidget {
               'Encontre o instrutor ideal para você'),
           buildAdvantage('Acesso vitalício', 'Aprenda no seu tempo'),
         ],
-      );
-    }
+      ),
+    );
   }
 }
